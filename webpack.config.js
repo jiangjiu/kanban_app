1d4bcdab23561120af0d4510fdaee94c7bf27fc1
zhangsiyuan <1021808625@qq.com>
2015-10-18 08:44:08 +0800

    Setting Up React

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,57 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH,'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
+var common = {
     entry:APP_PATH,
     output:{
         path:BUILD_PATH,
         filename:'bundle.js'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+
     plugins:[
         new HtmlwebpackPlugin({
             title: 'Kanban  app'
         })
-    ]
-
+    ],
+    module:{
+        loaders:[
+            {
+                test:/\.css$/,
+                loaders:['style','css'],
+                include:APP_PATH
+            },
+            {
+                test:/\.jsx?$/,
+                loaders:['babel'],
+                include:APP_PATH
+            }
+        ]
+    }
 };
+
+if(TARGET === 'start'||!TARGET) {
+    module.exports = merge(common, {
+        devtool:'eval-source-map',
+        devServer:{
+            historyApiFallback:true,
+            hot:true,
+            inline:true,
+            progress:true
+        },
+        plugins:[
+            new webpack.HotModuleReplacementPlugin()
+        ],
+
+    });
+}
