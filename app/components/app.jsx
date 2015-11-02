c9a16f98c3728881ee98f8f9643673328e159c0d
zhangsiyuan <1021808625@qq.com>
2015-11-02 22:13:06 +0800

    使用AltContainer代替@connect

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -2,39 +2,21 @@ import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions.js';
 import NoteStore from '../stores/NoteStore.js';
-import connect from '../decorators/connect.js';
+import AltContainer from 'alt/AltContainer.js';
 
-@connect(NoteStore)
 export default class App extends React.Component {
-    //constructor(props) {
-    //    super(props);
-    //    this.storeChanged = this.storeChanged.bind(this);
-    //    this.state = NoteStore.getState();
-    //
-    //}
-    //
-    //componentDidMount() {
-    //    NoteStore.listen(this.storeChanged);
-    //}
-    //
-    //componentWillUnmount() {
-    //    Notestore.unlisten(this.storeChanged);
-    //}
-    //
-    //storeChanged(state) {
-    //    this.setState(state);
-    //}
-
     render() {
-        const notes = this.state.notes;
-
         return (
             <div>
                 <button className='add-note' onClick={this.addNote}>+</button>
-                <Notes
-                    items={notes}
-                    onEdit={this.editNote}
-                    onDelete={this.deleteNote}/>
+                <AltContainer
+                    stores={{NoteStore}}
+                    inject={{
+                    items: () => NoteStore.getState().notes
+                    }}
+                >
+                <Notes onEdit={this.editNote} onDelete={this.deleteNote}/>
+                </AltContainer>
             </div>
         );
     }
